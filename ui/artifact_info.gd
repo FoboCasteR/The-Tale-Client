65f2817c949ef3820b28dde9b537c980eb92164a
Eugene Khutko <fobocaster@gmail.com>
2022-05-30 11:08:18 +0300

    Show integrity percent

--- ui/artifact_info.gd
+++ ui/artifact_info.gd
@@ -3,7 +3,9 @@ extends Control
 export(NodePath) onready var name_label = get_node(name_label)
 export(NodePath) onready var physical_power_label = get_node(physical_power_label)
 export(NodePath) onready var magical_power_label = get_node(magical_power_label)
-export(Array, Color) var rarity_colors
+export(NodePath) onready var integrity_label = get_node(integrity_label)
+export(Array, Color) var rarity_colors = [Color.white, Color.dodgerblue, Color.blueviolet]
+export(Array, Color) var integrity_colors = [Color.white, Color.orange, Color.orangered, Color.red]
 
 var artifact: Artifact setget set_artifact
 
@@ -20,6 +22,10 @@ func update_ui():
 	if artifact.is_equipment:
 		physical_power_label.text = str(artifact.physical_power)
 		magical_power_label.text = str(artifact.magical_power)
+
+		var integrity_percent = floor(float(artifact.integrity) / artifact.max_integrity * 10000) / 100
+		integrity_label.text = "%.*f%%" % [2 if integrity_percent < 100 else 0, integrity_percent]
+		integrity_label.add_color_override("font_color", integrity_color(integrity_percent))
 
 		var tooltip_items = PoolStringArray(
 			[
@@ -40,3 +46,16 @@ func update_ui():
 			tooltip_items.append("Бонус: %s" % bonuses.join(", "))
 
 		hint_tooltip = tooltip_items.join("\n")
+
+
+func integrity_color(percent: float):
+	var i = 0
+
+	if percent < 90:
+		i = 1
+	elif percent < 83:
+		i = 2
+	elif percent < 80:
+		i = 3
+
+	return integrity_colors[i]
